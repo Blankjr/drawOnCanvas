39d5945618d972d28106eb6e5dfd246835d8c92b
christian.kuerpig <christian.kuerpig@study.hs-duesseldorf.de>
2020-11-09 22:11:04 +0100

    init

---
+++ buttons.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+function clearDrawing() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  ctx.restore();
+  drawBorder();
+}
